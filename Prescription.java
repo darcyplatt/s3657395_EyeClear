f978fba6b9703f3ddc9372ebf0afeabf3e290187
darcyplatt <s3657395@student.rmit.edu.au>
2024-10-23 21:52:02 +1100

    function logic

--- Prescription.java
+++ Prescription.java
@@ -1,21 +1,39 @@
 package s3657395_EyeClear;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Prescription {
-        private int prescID;
+        private static final HashSet<String> usedIDs = new HashSet<>(); // Static collection to track all used ID
+        private String prescID;
         private String firstName;
         private String lastName;
         private String address;
         private float sphere;
         private float axis;
         private float cylinder;
-        private Date examinationDate;
+        private String examinationDate;
         private String optometrist;
-        private String[] remarkTypes = { "Client", "Optometrist" }; //change this to a ENUM
+        private String[] remarkTypes = { "Client", "Optometrist" }; // change this to a ENUM
         private ArrayList<String> postRemarks = new ArrayList<>();
+
+        Prescription() {
+                this.prescID = "000";
+                this.firstName = "";
+                this.lastName = "";
+                this.address = "";
+                this.sphere = -99f;
+                this.axis = -99f;
+                this.cylinder = -99f;
+                this.examinationDate = "";
+                this.optometrist = "";
+        }
 
         public boolean addPrescription() {
                 // TODO Add the prescription's information to a TXT file
@@ -26,96 +44,112 @@ public class Prescription {
                 // the information should not be added to the TXT file, and the function should
                 // return false.
 
-                if(validPrescription()) {
+                Scanner scanner = new Scanner(System.in);
+                setFirstName(scanner.nextLine());
+                setLastName(scanner.nextLine());
+                setAddress(scanner.nextLine());
+                setSphere(scanner.nextFloat());
+                setAxis(scanner.nextFloat());
+                setCylinder(scanner.nextFloat());
+                setDate(scanner.nextLine());
+                scanner.close();
+
+                if (validPrescription()) {
                         generateID();
-                        writePrescription(this.prescID, this.firstName, this.lastName, this.address, this.sphere,
-                                this.axis, this.cylinder, this.examinationDate, this.optometrist);
+                        writePrescription();
+                } else {
+                        throw new IllegalArgumentException("Invalid prescription values");
                 }
                 return validPrescription();
         }
 
-        public boolean setName(String firstName, String lastName) {
-                if (isValidName(firstName)) {
+        private boolean setFirstName(String firstName) {
+                if (firstName != null && firstName.length() >= 4 && firstName.length() <= 15) {
                         this.firstName = firstName;
-                } else {
-                        throw new IllegalArgumentException("Invalid first name");
                 }
-                if (isValidName(lastName)) {
+                return firstName != null && firstName.length() >= 4 && firstName.length() <= 15;
+        }
+
+        private boolean setLastName(String lastName) {
+                if (lastName != null && lastName.length() >= 4 && lastName.length() <= 15) {
                         this.lastName = lastName;
-                } else {
-                        throw new IllegalArgumentException("Invalid last name");
                 }
-                return isValidName(firstName) && isValidName(lastName);
+                return lastName != null && lastName.length() >= 4 && lastName.length() <= 15;
         }
 
-        private boolean isValidName(String name) {
-                return name != null && name.length() >= 4 && name.length() <= 15;
-        }
-
-        public boolean setAddress(String address) {
+        private boolean setAddress(String address) {
                 if (address != null && address.length() >= 20) {
                         this.address = address;
-                } else {
-                        throw new IllegalArgumentException("Invalid address");
                 }
                 return address != null && address.length() >= 20;
         }
 
-        public boolean setSphere(float sphere) {
+        private boolean setSphere(float sphere) {
                 if (sphere >= -20.00f && sphere <= 20.00f) {
                         this.sphere = sphere;
-                } else {
-                        throw new IllegalArgumentException("Invalid sphere value");
                 }
                 return sphere >= -20.00f && sphere <= 20.00f;
         }
 
-        public boolean setCylinder(float cylinder) {
+        private boolean setCylinder(float cylinder) {
                 if (cylinder >= -4.00f && cylinder <= 4.00f) {
                         this.cylinder = cylinder;
-                } else {
-                        throw new IllegalArgumentException("Invalid cylinder value");
                 }
                 return cylinder >= -4.00f && cylinder <= 4.00f;
         }
 
-        public boolean setAxis(float axis) {
+        private boolean setAxis(float axis) {
                 if (axis >= 0.00f && axis <= 180.00f) {
                         this.axis = axis;
-                } else {
-                        throw new IllegalArgumentException("Invalid axis value");
                 }
                 return axis >= 0.00f && axis <= 180.00f;
         }
 
-        public boolean setDate(Date examinationDate) {
-                //inspect the Date object for a method to check for correct formatting DD/MM/YY
-                this.examinationDate = examinationDate;
-                return true;
+        private boolean setDate(String examinationDate) {
+                try {
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+                        LocalDate date = LocalDate.parse(examinationDate, formatter);
+
+                        String formattedDate = date.format(formatter);
+                        this.examinationDate = formattedDate;
+                        return true;
+
+                } catch (DateTimeParseException e) {
+                        return false;
+                }
         }
 
-        public boolean setOptometrist(String optometrist) {
-                if(optometrist.length() >= 8 && optometrist.length() <= 25) {
+        private boolean setOptometrist(String optometrist) {
+                if (optometrist.length() >= 8 && optometrist.length() <= 25) {
                         this.optometrist = optometrist;
-                } else {
-                        throw new IllegalArgumentException("Invalid optometrist value");
                 }
                 return optometrist.length() >= 8 && optometrist.length() <= 25;
         }
 
-        public void generateID() {
-                        int id = 1;
-                        this.prescID = id;
+        private void generateID() {
+                String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                Random random = new Random();
+                StringBuilder newID;
+                do {
+                        newID = new StringBuilder();
+                        // Generate a 6-character string
+                        for (int i = 0; i < 6; i++) {
+                                int index = random.nextInt(characters.length());
+                                newID.append(characters.charAt(index));
+                        }
+                } while (!usedIDs.add(newID.toString())); // Keep generating until we get a unique ID
+
+                this.prescID = newID.toString();
         }
 
         private boolean validPrescription() {
-                return setName(firstName, lastName) && setAddress(address) && setSphere(sphere) &&
-                        setCylinder(cylinder) && setAxis(axis) && setDate(examinationDate) && setOptometrist(optometrist);
+                return setFirstName(this.firstName) && setLastName(this.lastName) && setAddress(this.address)
+                                && setSphere(this.sphere) && setCylinder(this.cylinder) && setAxis(this.axis)
+                                && setDate(this.examinationDate) && setOptometrist(this.optometrist);
         }
-        
-        private void writePrescription(int prescID, String firstName, String lastName, String address,
-                        float sphere, float axis, float cylinder, Date examinationDate, String optometrist) {
-                //write data to .TXT file
+
+        private void writePrescription() {
+                // write data to .TXT file
         }
 
         public boolean addRemark() {
@@ -126,15 +160,30 @@ public class Prescription {
                 // If the remark does not meet the given conditions,
                 // the information should not be added to the TXT file, and the function should
                 // return false.
-                return true;
+
+                Scanner scanner = new Scanner(System.in);
+                String type = scanner.nextLine();
+                String text = scanner.nextLine();
+                scanner.close();
+
+                if (validRemarkType(type) && validRemarkText(text) && postRemarks.size() < 2) {
+                        String remark = type + " - " + text;
+                        postRemarks.add(remark);
+                }
+
+                return validRemarkType(type) && validRemarkText(text) && postRemarks.size() <= 2;
         }
 
-        public boolean validRemarkType(String type) {
+        private boolean validRemarkType(String type) {
                 return type.equalsIgnoreCase(remarkTypes[0]) || type.equalsIgnoreCase(remarkTypes[1]);
         }
 
-        public boolean validRemarkText(String text) {
+        private boolean validRemarkText(String text) {
                 return Pattern.matches("^[A-Z]\\w*(?:\\s+\\w+){5,19}$", text.trim());
+        }
+
+        private void writeRemark() {
+
         }
 
 }
